3fee0e2f4da523c2528128d4ed73f476bb4c4952
nanabernardo <nanabernardo@gmail.com>
2025-08-31 12:51:48 -0300

    feat: add responsive menu toggle with hamburger button

---
+++ script.js
@@ -0,0 +1,6 @@
+const menuToggler = document.querySelector(".menu-toggler");
+const navbar = document.querySelector(".navbar");
+
+menuToggler.addEventListener("click", () => {
+  navbar.classList.toggle("show");
+});
